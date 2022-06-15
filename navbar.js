23966e11e5000b62998831113204a276348a5322
visheshguria <vishesh.guriaa94@gmail.com>
2022-06-15 22:43:52 +0530

    navbar refrence pending

---
+++ navbar.js
@@ -0,0 +1,36 @@
+document.querySelector("#logo img").addEventListener("click", logofunc)
+
+function logofunc() {
+   
+}
+
+doucument.querySelector("#create").addEventListener("click", createfunction);
+
+function createfunction() {
+
+}
+
+doucument.querySelector("#pricing").addEventListener("click", pricingfunction);
+
+function pricingfunction() {
+
+}
+
+
+document.querySelector("#enterprise").addEventListener("click", enterprisefunction);
+
+function enterprisefunction() {
+
+}
+
+doucument.querySelector("#casestudy").addEventListener("click", casefunction);
+
+function casefunction() {
+
+}
+
+doucument.querySelector("#btn1").addEventListener("click", loginfunction);
+
+function loginfunction() {
+    
+}
