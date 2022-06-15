5ff4144c7b3c73de2ac8e7ebe7ad94fd04417915
visheshguria <vishesh.guriaa94@gmail.com>
2022-06-15 23:00:53 +0530

    format done

--- navbar.js
+++ navbar.js
@@ -1,41 +1,41 @@
 document.querySelector("#logo img").addEventListener("click", logofunc)
 
 function logofunc() {
-    window.location.href="index.html"
+    window.location.href = "index.html"
 }
 
 doucument.querySelector("#create").addEventListener("click", createfunction);
 
 function createfunction() {
-    window.location.href="create.html"
+    window.location.href = "create.html"
 }
 
 doucument.querySelector("#pricing").addEventListener("click", pricingfunction);
 
 function pricingfunction() {
-    window.location.href="pricing.html"
+    window.location.href = "pricing.html"
 }
 
 document.querySelector("#enterprise").addEventListener("click", enterprisefunction);
 
 function enterprisefunction() {
-    window.location.href="enterprise.html"
+    window.location.href = "enterprise.html"
 }
 
 doucument.querySelector("#casestudy").addEventListener("click", casefunction);
 
 function casefunction() {
-    window.location.href="casestudy.html"
+    window.location.href = "casestudy.html"
 }
 
 doucument.querySelector("#btn1").addEventListener("click", loginfunction);
 
 function loginfunction() {
-    window.location.href="login.html"
+    window.location.href = "login.html"
 }
 
 doucument.querySelector("#btn2").addEventListener("click", signupfunction);
 
 function loginfunction() {
-    window.location.href="signup.html"
+    window.location.href = "signup.html"
 }
